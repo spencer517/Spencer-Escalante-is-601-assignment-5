7a8976dce2b220ab7111ee50138f5234d0e89ea2
spenc <spencer517@msn.com>
2021-07-25 19:57:27 -0400

    initial view setup

--- app/app.py
+++ app/app.py
@@ -1,41 +1,45 @@
 from typing import List, Dict
-import mysql.connector
 import simplejson as json
-from flask import Flask, Response, render_template
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
 
 
-def heightweight_import() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'heightWeight'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(dictionary=True)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'heightWeight'
+mysql.init_app(app)
 
-    cursor.execute('SELECT * FROM tableHeightWeight')
-    result = cursor.fetchall()
 
-    cursor.close()
-    connection.close()
-
-    return result
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     user = {'username' : 'Spencer'}
-    heightWeight = heightweight_import()
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tableHeightWeight')
+    heightWeight = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, heightsAndWeights = heightWeight)
 
-@app.route('/api/heightWeight')
-def heightsAndWeights() -> str:
-    js = json.dumps(heightweight_import())
+@app.route('/api/v1/heightWeight')
+def heightsAndWeights(patient_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tableHeightWeight WHERE id=%s', patient_id)
+    result = cursor.fetchall()
+    js = json.dumps(result)
     resp = Response(js, status=200, mimetype='application/json')
     return resp
+
+@app.route('/view/<int:patient_id>', methods=['GET'])
+def rec_view(patient_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tableHeightWeight WHERE id=%s', patient_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='Home', patient=result[0])
 
 
 if __name__ == '__main__':
